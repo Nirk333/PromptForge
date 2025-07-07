f9f599eaeefaab4734cd585b824fc5d130426c14
Nirk333 <kvaish0713@gmail.com>
2025-07-07 21:09:17 +0200

    Scaffold generate_sitemap.py script

---
+++ scripts/generate_sitemap.py
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/env python3
+"""
+generate_sitemap.py
+
+Auto-generate sitemap.xml for PromptHive.
+"""
+
+def generate_sitemap():
+    # TODO: implement logic to crawl docs/ and output sitemap.xml
+    pass
+
+if __name__ == "__main__":
+    generate_sitemap()
