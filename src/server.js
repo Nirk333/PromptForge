4ae05004431fdc8f96a8f0df1f0191d3a2929ace
Nirk333 <kvaish0713@gmail.com>
2025-07-07 22:22:28 +0200

    Mount /api/prompts router in server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+// Import the prompts router
+
+const promptsRouter = require('./routes/prompts');
 
 app.get('/', (req, res) => {
   res.send('PromptForge API is up and running!');
 });
+// Mount the prompts router
+
+app.use('/api/prompts', promptsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
