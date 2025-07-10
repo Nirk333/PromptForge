6208405f7c40bfe8f2015d46a4b67919899d3aa5
Nirk333 <kvaish0713@gmail.com>
2025-07-10 11:49:40 +0200

    Remove unused header wrapper from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} ${robotoMono.variable} antialiased`}>
-        
-        {children}
+                {children}
       </body>
     </html>
   );
